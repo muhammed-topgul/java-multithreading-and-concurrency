001b7b3f2e84a9afdc6fb9e7bd9f1b7c0dfa19be
muhammed-topgul <muhammedtopgul.29@gmail.com>
2021-12-18 02:35:16 +0300

    002. threads synchronization.

---
+++ src/main/java/com/muhammedtopgul/ch01/synchronization/Counter.java
@@ -0,0 +1,32 @@
+package com.muhammedtopgul.ch01.synchronization;
+
+/**
+ * @author muhammed-topgul
+ * @since at 18.12.2021 02:18
+ */
+
+public class Counter {
+
+    private int count = 0;
+    private int bound = 100;
+
+    public Counter(int bound) {
+        this.bound = bound;
+    }
+
+    public synchronized void increaseCount() {
+        for (int i = 0; i < bound; i++) {
+            count++;
+        }
+    }
+
+    public synchronized void decreaseCount() {
+        for (int i = 0; i < bound; i++) {
+            count--;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
---
+++ src/main/java/com/muhammedtopgul/ch01/synchronization/ThreadSafe.java
@@ -0,0 +1,27 @@
+package com.muhammedtopgul.ch01.synchronization;
+
+/**
+ * @author muhammed-topgul
+ * @since at 18.12.2021 02:07
+ */
+
+public class ThreadSafe {
+
+    public static void main(String[] args) {
+        Counter counter = new Counter(500_000);
+
+        Thread thread1 = new Thread(() -> counter.increaseCount());
+        thread1.start();
+        Thread thread2 = new Thread(() -> counter.increaseCount());
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Count: " + counter.getCount());
+    }
+}
